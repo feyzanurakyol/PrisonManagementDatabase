91d4fb32452d454bf35794cfddca1c8a85108293
Feyza Nur Akyol <fnuakyol@gmail.com>
2021-06-06 21:33:55 +0300

    Update joins.sql

--- joins.sql
+++ joins.sql
@@ -1,2 +1,17 @@
+SELECT p.prison_id, p.`name`, a.doctor_id, a.created_date
+FROM prison AS p
+LEFT OUTER JOIN appointment AS a
+ON p.prison_id = a.prison_id ;
+
+
+SELECT prison.prison_id, prison.`name`, visit.visitor_tc, visit.visit_date
+FROM visit 
+RIGHT OUTER JOIN prison
+ON prison.prison_id = visit.prison_id ;
+
+SELECT prison.prison_id, prison.`name`, visit.visitor_tc, visit.visit_date
+FROM visit 
+JOIN prison
+ON prison.prison_id = visit.prison_id ;
 
 
